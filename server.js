1c3d3699450bd30ad606d97654b1ab4a74d826f3
wagnerxd12 <wagnersanches10@hotmail.com>
2025-03-04 16:39:28 -0300

    novos arquivos e mundaças no projeto

--- server.js
+++ server.js
@@ -9,11 +9,26 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts") // Added express-ejs-layouts
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+
+app.set("view engine", "ejs") // Setting view engine to ejs
+app.use(expressLayouts) // Using express-ejs-layouts
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+
+// Index route
+app.get('/', function (req, res) {
+  res.render("index", { title: "Home" });
+});
 
 /* ***********************
  * Local Server Information
@@ -28,3 +43,5 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
